e4ed06adab5514847ee0e97fdff3b225c4d98be5
MasaKusu <masa.kusuda.1999@gmail.com>
2022-04-27 10:37:27 +0900

    bba971c

---
+++ _nuxt/static/1651023441/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"gh_nuxt_test\u002F_nuxt\u002Fstatic\u002F1651023441",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"gh_nuxt_test\u002F",assetsPath:"gh_nuxt_test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
